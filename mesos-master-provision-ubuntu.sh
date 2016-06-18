37d4c2286619aade4e66bde61c8c6b52a3569b2b
yufanliu <Yufan-l@users.noreply.github.com>
2016-06-17 17:24:50 -0700

    mesos-master-provision-ubuntu.sh

---
+++ mesos-master-provision-ubuntu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+zk=$1
+ip=$2
+
+if [ -z "$zk" ];then
+ echo "please provide zk info"
+ exit
+fi
+
+if [ -z "$ip" ];then
+ echo "please provide slave ip"
+ exit
+fi
+
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+sudo apt-get install mesos marathon
+
+echo $zk >  /etc/mesos/zk
+echo $ip > /etc/mesos-master/hostname
+echo $ip > /etc/mesos-master/ip
+
+sudo service mesos-slave stop
+sudo sh -c "echo manual > /etc/init/mesos-slave.override"
+
+sudo service mesos-master restart
