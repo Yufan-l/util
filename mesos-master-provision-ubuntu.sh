2048afddf76a34e20affcf6739282797742b4d3f
yufanliu <Yufan-l@users.noreply.github.com>
2016-06-20 12:03:32 -0700

    change java vendor

--- mesos-master-provision-ubuntu.sh
+++ mesos-master-provision-ubuntu.sh
@@ -12,9 +12,9 @@ if [ -z "$ip" ];then
  exit
 fi
 
-sudo apt-add-repository ppa:webupd8team/java
+sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get -y update
-sudo apt-get -y install oracle-java8-installer
+sudo apt-get -y install openjdk-8-jdk
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
@@ -23,7 +23,7 @@ CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 
-sudo apt-get install mesos marathon
+sudo apt-get -y install mesos marathon
 
 echo $zk >  /etc/mesos/zk
 echo $ip > /etc/mesos-master/hostname
@@ -33,3 +33,4 @@ sudo service mesos-slave stop
 sudo sh -c "echo manual > /etc/init/mesos-slave.override"
 
 sudo service mesos-master restart
+sudo service marathon restart
