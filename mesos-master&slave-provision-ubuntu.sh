b5f46baa9c231fb60e126b7544aff650322d269b
yufanliu <Yufan-l@users.noreply.github.com>
2016-06-20 11:38:18 -0700

    Update mesos-master&slave-provision-ubuntu.sh

--- mesos-master&slave-provision-ubuntu.sh
+++ mesos-master&slave-provision-ubuntu.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+zk=$1
+ip=$2
 
+if [ -z "$zk" ];then
+ echo "please provide zk info"
+ exit
+fi
+
+if [ -z "$ip" ];then
+ echo "please provide slave ip"
+ exit
+fi
+
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+sudo apt-get install mesos marathon
+
+echo $zk >  /etc/mesos/zk
+echo $ip > /etc/mesos-master/hostname
+echo $ip > /etc/mesos-master/ip
+echo 'docker,mesos' > /etc/mesos-slave/containerizers
+echo $ip > /etc/mesos-slave/hostname
+echo $ip > /etc/mesos-slave/ip
+echo '10mins' > /etc/mesos-slave/executor_registration_timeout
+
+
+sudo service mesos-master restart
+sudo service mesos-slave restart
