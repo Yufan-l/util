865d224230f130788ad803ebed2f871032e2a9ef
yufanliu <Yufan-l@users.noreply.github.com>
2016-06-20 12:21:18 -0700

    Update mesos-slave-provision-ubuntu.sh

--- mesos-slave-provision-ubuntu.sh
+++ mesos-slave-provision-ubuntu.sh
@@ -12,6 +12,9 @@ if [ -z "$ip" ];then
  exit
 fi
 
+sudo apt-get-repository -y ppa:openjdk-r/ppa
+sudo apt-get -y update
+sudo apt-get -y install openjdk-8-jdk
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
@@ -20,7 +23,7 @@ CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 
-sudo apt-get install mesos
+sudo apt-get -y install mesos
 
 sudo service zookeeper stop
 sudo sh -c "echo manual > /etc/init/zookeeper.override"
