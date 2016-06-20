5c7475ddd5fc1611e3404ec6013f923df780fea9
yufanliu <Yufan-l@users.noreply.github.com>
2016-06-20 11:21:45 -0700

    force install

--- docker-provision-ubuntu14.sh
+++ docker-provision-ubuntu14.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
-sudo apt-get update
-sudo apt-get install apparmor
-sudo apt-get install docker-engine
+sudo apt-get -y update
+sudo apt-get -y install apparmor
+sudo apt-get -y install docker-engine
 sudo service docker start
 
 
